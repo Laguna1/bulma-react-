fab49cf4911b06b3c202b09bfdbc1c24e4daabb8
Laguna1 <lagunazoomarket@gmail.com>
2021-08-01 01:55:14 +0300

    Change node-sass -v, add bulma & some css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">Bulma</h1>
+<p className="subtitle">
+  Modern CSS framework based on{' '}
+  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">
+    Flexbox
+  </a>
+</p>
+
+<div className="field">
+  <div className="control">
+    <input className="input" type="text" placeholder="Input" />
+  </div>
+</div>
+
+<div className="field">
+  <p className="control">
+    <span className="select">
+      <select>
+        <option>Select dropdown</option>
+        <option>a</option>
+        <option>b</option>
+        <option>c</option>
+      </select>
+    </span>
+  </p>
+</div>
+
+<div className="btns is-center">
+  <button className="button is-primary">Primary</button>
+  <button className="button is-link">Link</button>
+</div>
+      </div>
+    );
+  }
 }
 
 export default App;
